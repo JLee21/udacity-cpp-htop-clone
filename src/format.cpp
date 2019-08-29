a828e28b4d06c0980b9e265e197ce0ad1d225793
root <asdf>
2019-08-29 05:57:55 +0000

    update

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,19 @@
 #include <string>
-
 #include "format.h"
+#include <iostream>
 
 using std::string;
 
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+    int hours = seconds / 3600;
+    int remaining_seconds = seconds - (hours * 3600);
+    int mins = remaining_seconds / 60;
+    int secs = seconds - (hours * 3600 + mins * 60);
+  
+  	char output[20];
+    sprintf(output, "%02u:%02u:%02u", hours, mins, secs);
+  	return output;
+}
--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <string>
 #include <vector>
+#include <iostream>
 
 #include "linux_parser.h"
 
@@ -9,9 +10,11 @@ using std::stof;
 using std::string;
 using std::to_string;
 using std::vector;
+using std::cout;
+using std::endl;
 
-// DONE: An example of how to read data from the filesystem
 string LinuxParser::OperatingSystem() {
+  // Parse a file's line for this --> PRETTY_NAME="Ubuntu 16.04.5 LTS"
   string line;
   string key;
   string value;
@@ -33,20 +36,20 @@ string LinuxParser::OperatingSystem() {
   return value;
 }
 
-// DONE: An example of how to read data from the filesystem
 string LinuxParser::Kernel() {
-  string os, kernel;
+  string os, kernel, version;
   string line;
   std::ifstream stream(kProcDirectory + kVersionFilename);
   if (stream.is_open()) {
     std::getline(stream, line);
     std::istringstream linestream(line);
-    linestream >> os >> kernel;
+    linestream >> os >> version >> kernel;
   }
   return kernel;
 }
 
 // BONUS: Update this to use std::filesystem
+// return a vector of ints that denote all of the PIDs
 vector<int> LinuxParser::Pids() {
   vector<int> pids;
   DIR* directory = opendir(kProcDirectory.c_str());
@@ -67,10 +70,41 @@ vector<int> LinuxParser::Pids() {
 }
 
 // TODO: Read and return the system memory utilization
-float LinuxParser::MemoryUtilization() { return 0.0; }
+float LinuxParser::MemoryUtilization() { 
+  // In my implementation, I will calculate:
+  // Non cache/buffer memory (green) which equals Total used memory - (Buffers + Cached memory)
+  string line;
+  string key;
+  string value;
+  
+  std::ifstream filestream(kProcDirectory+kMeminfoFilename);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+      while (linestream >> key >> value) {
+        if (key == "PRETTY_NAME") {
+          std::replace(value.begin(), value.end(), '_', ' ');
+          return value;
+        }
+      }
+    }
+  }
+  return value;
+}
 
-// TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+long LinuxParser::UpTime() { 
+  string uptime, idletime, line;
+  std::ifstream stream(kProcDirectory + kUptimeFilename);
+  if (stream.is_open()) {
+    std::getline(stream, line);
+    std::istringstream linestream(line);
+    linestream >> uptime >> idletime;
+  }
+  // how to convert a string to a number (long)?
+  // kernelhttps://www.geeksforgeeks.org/converting-strings-numbers-cc/
+  // or use stof from std lib
+   return std::stof(uptime);
+}
 
 // TODO: Read and return the number of jiffies for the system
 long LinuxParser::Jiffies() { return 0; }
@@ -88,11 +122,42 @@ long LinuxParser::IdleJiffies() { return 0; }
 // TODO: Read and return CPU utilization
 vector<string> LinuxParser::CpuUtilization() { return {}; }
 
-// TODO: Read and return the total number of processes
-int LinuxParser::TotalProcesses() { return 0; }
+int LinuxParser::TotalProcesses() { 
+  // IMPROVEMENT: abstract this funtion since it is similar to TotalProcesses and RunningProcesses
+  // Parse a file's line for this --> processes 1234
+  string line;
+  string key;
+  string value;
+  std::ifstream filestream(kProcDirectory + kStatFilename);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+      while (linestream >> key >> value) {
+        if (key == "processes") {        
+          return std::stoi(value);
+        }
+      }
+    }
+  }
+}
 
-// TODO: Read and return the number of running processes
-int LinuxParser::RunningProcesses() { return 0; }
+int LinuxParser::RunningProcesses() { 
+  // Parse a file's line for this --> procs_running 1234
+  string line;
+  string key;
+  string value;
+  std::ifstream filestream(kProcDirectory + kStatFilename);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+      while (linestream >> key >> value) {
+        if (key == "procs_running") {        
+          return std::stoi(value);
+        }
+      }
+    }
+  }
+}
 
 // TODO: Read and return the command associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
